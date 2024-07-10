8ffeb327fb540e59a64c4f2202f783550fc72c71
Mykola Dotsenko <130664123+MykolaDotsenko@users.noreply.github.com>
2024-07-10 21:53:18 +0300

    filter bar

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -6,27 +6,58 @@ export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
+  const [genre, setGenre] = useState("All Genres");
+  const [rating, setRating] = useState("All");
+
   useEffect(() => {
     fetch("movies.json")
       .then((response) => response.json())
       .then((data) => setMovies(data));
   }, []);
 
-
-const handleSearchChange = (e) => {
+  const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
   };
 
-
-const filteredMovies = movies.filter(movie => 
-
+  const filteredMovies = movies.filter((movie) =>
     movie.title.toLowerCase().includes(searchTerm.toLowerCase())
-)
-
+  );
 
   return (
     <div>
-      <input type="text" className="search-input" placeholder="Search movies..." value={searchTerm}  onChange={handleSearchChange} />
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search movies..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+
+      <div className="filter-bar">
+<div className="filter-slot">
+<label>Genre</label>
+<select className="filter-dropdown">
+<option>All Genres</option>
+<option>Action</option>
+<option>Drama</option>
+<option>Fantasy</option>
+<option>Horror</option>
+</select>
+
+</div>
+
+<div className="filter-slot">
+<label>Rating</label>
+<select className="filter-dropdown">
+<option>All</option>
+<option>Good</option>
+<option>Ok</option>
+<option>Bad</option>
+
+</select>
+</div>
+      </div>
+
       <div className="movies-grid">
         {filteredMovies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />

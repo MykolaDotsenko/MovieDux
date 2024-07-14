53b754ed9376e22b9cadb501693cf96d126ed983
Mykola Dotsenko <130664123+MykolaDotsenko@users.noreply.github.com>
2024-07-14 12:00:54 +0300

    watchlist router

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,24 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../styles.css";
 import MovieCard from "./MovieCard";
 
-export default function MoviesGrid() {
-  const [movies, setMovies] = useState([]);
+export default function MoviesGrid({ movies, watchlist, toggleWatchlist }) {
   const [searchTerm, setSearchTerm] = useState("");
 
   const [genre, setGenre] = useState("All Genres");
   const [rating, setRating] = useState("All");
 
-  useEffect(() => {
-    fetch("movies.json")
-      .then((response) => response.json())
-      .then((data) => setMovies(data));
-  }, []);
-
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
   };
-  
+
   const handleGenreChange = (e) => {
     setGenre(e.target.value);
   };
@@ -27,39 +20,41 @@ export default function MoviesGrid() {
     setRating(e.target.value);
   };
 
-const matchesGenre = (movie, genre) => {
-return genre === "All Genres" ||  movie.genre.toLowerCase() === genre.toLowerCase();
+  const matchesGenre = (movie, genre) => {
+    return (
+      genre === "All Genres" ||
+      movie.genre.toLowerCase() === genre.toLowerCase()
+    );
+  };
 
-}
+  const matchesSerchTerm = (movie, searchTerm) => {
+    return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
 
-const matchesSerchTerm = (movie, searchTerm) => {
-  return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
-}
+  const matchesRating = (movie, rating) => {
+    switch (rating) {
+      case "All":
+        return true;
 
-const matchesRating = (movie, rating) => {
-  switch (rating) {
-    case "All":
-    return true;
+      case "Good":
+        return movie.rating >= 8;
 
-    case "Good":
-      return movie.rating >=8;
+      case "Ok":
+        return movie.rating >= 5 && movie.rating < 8;
 
-      case 'Ok':
-      return movie.rating >=5 && movie.rating < 8;
+      case "Bad":
+        return movie.rating < 5;
 
-      case 'Bad':
-      return movie.rating < 5;
+      default:
+        return false;
+    }
+  };
 
-
-    default:
-      return false
-  }
-}
-
-  const filteredMovies = movies.filter((movie) =>
-    matchesGenre(movie, genre) && 
-  matchesRating(movie, rating) &&
-  matchesSerchTerm(movie, searchTerm)
+  const filteredMovies = movies.filter(
+    (movie) =>
+      matchesGenre(movie, genre) &&
+      matchesRating(movie, rating) &&
+      matchesSerchTerm(movie, searchTerm)
   );
 
   return (
@@ -73,33 +68,39 @@ const matchesRating = (movie, rating) => {
       />
 
       <div className="filter-bar">
-<div className="filter-slot">
-<label>Genre</label>
-<select className="filter-dropdown" value={genre} onChange={handleGenreChange}>
-<option>All Genres</option>
-<option>Action</option>
-<option>Drama</option>
-<option>Fantasy</option>
-<option>Horror</option>
-</select>
+        <div className="filter-slot">
+          <label>Genre</label>
+          <select
+            className="filter-dropdown"
+            value={genre}
+            onChange={handleGenreChange}
+          >
+            <option>All Genres</option>
+            <option>Action</option>
+            <option>Drama</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+          </select>
+        </div>
 
-</div>
-
-<div className="filter-slot">
-<label>Rating</label>
-<select className="filter-dropdown" value={rating} onChange={handleRatingChange}>
-<option>All</option>
-<option>Good</option>
-<option>Ok</option>
-<option>Bad</option>
-
-</select>
-</div>
+        <div className="filter-slot">
+          <label>Rating</label>
+          <select
+            className="filter-dropdown"
+            value={rating}
+            onChange={handleRatingChange}
+          >
+            <option>All</option>
+            <option>Good</option>
+            <option>Ok</option>
+            <option>Bad</option>
+          </select>
+        </div>
       </div>
 
       <div className="movies-grid">
         {filteredMovies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
+          <MovieCard movie={movie} key={movie.id} toggleWatchlist={toggleWatchlist} isWatchlisted={watchlist.includes(movie.id)} />
         ))}
       </div>
     </div>
